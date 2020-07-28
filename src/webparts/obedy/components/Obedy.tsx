edea858fca2b1d458a5f4a5286bc636a1a4250f1
Samuel <samuelorth62@gmail.com>
2020-07-28 10:53:01 +0200

    divs for restaurants

--- src/webparts/obedy/components/Obedy.tsx
+++ src/webparts/obedy/components/Obedy.tsx
@@ -3,20 +3,48 @@ import styles from './Obedy.module.scss';
 import { IObedyProps } from './IObedyProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
+import BeQuick from "./Restaurants/BeQuick";
+import HanoiGarden from "./Restaurants/HanoiGarden";
+import Mlyn from "./Restaurants/Mlyn";
+import Rebecca from "./Restaurants/Rebecca";
+import Rotunda from "./Restaurants/Rotunda";
+import Yummy from "./Restaurants/Yummy";
+
+
 export default class Obedy extends React.Component<IObedyProps, {}> {
   public render(): React.ReactElement<IObedyProps> {
     return (
-      <div className={ styles.obedy }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
+      <div className={styles.obedy}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.column}>
+              <span className={styles.title}>Welcome to SharePoint!</span>
+              <p className={styles.subTitle}>Customize SharePoint experiences using Web Parts.</p>
+              <p className={styles.description}>{escape(this.props.description)}</p>
+              <a href="https://aka.ms/spfx" className={styles.button}>
+                <span className={styles.label}>Learn more</span>
               </a>
             </div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.column}>
+              <Rotunda/>
+            </div>
+            <div className={styles.column}>
+              <Mlyn/>
+            </div>
+          </div>
+          <div className={styles.row}>
+            <BeQuick/>
+          </div>
+          <div className={styles.row}>
+            <Yummy/>
+          </div>
+          <div className={styles.row}>
+            <Rebecca/>
+          </div>
+          <div className={styles.row}>
+            <HanoiGarden/>
           </div>
         </div>
       </div>
---
+++ src/webparts/obedy/components/Restaurants/Rotunda.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import styles from '../Obedy.module.scss';
+import { IObedyProps } from '../IObedyProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+export default class Rotunda extends React.Component {
+  public render(): React.ReactElement {
+    return (
+      <div className={styles.obedy}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.column}>
+              <span className={styles.title}>Rotunda</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
