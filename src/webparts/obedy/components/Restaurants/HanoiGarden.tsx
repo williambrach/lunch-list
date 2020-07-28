853a15ce19d645f3a052935df5a07eec3ebd4858
William Brach <William.Brach@snt.sk>
2020-07-28 13:31:18 +0200

    fixing header text and adding hanoi logic

--- src/webparts/obedy/components/Restaurants/HanoiGarden.tsx
+++ src/webparts/obedy/components/Restaurants/HanoiGarden.tsx
@@ -4,6 +4,30 @@ import { IObedyProps } from '../IObedyProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
 export default class HanoiGarden extends React.Component {
+
+
+  getMenuByDate(type: number): string {
+
+    let weekMap = new Map();
+
+    weekMap.set(1, 'pondelok');
+    weekMap.set(2, 'utorok');
+    weekMap.set(3, 'streda');
+    weekMap.set(4, 'stvrtok');
+    weekMap.set(5, 'piatok');
+
+    const dayNum = new Date(Date.now());
+    const day = weekMap.get(dayNum.getDay())
+
+
+    if (type == 1){
+      return "../src/assets/hanoi_"+day+"_1.pdf"
+    }
+    else {
+      return "../src/assets/hanoi_"+day+"_2.pdf"
+    }
+}
+
   public render(): React.ReactElement {
     return (
       <div className={styles.obedy}>
@@ -12,9 +36,9 @@ export default class HanoiGarden extends React.Component {
             <div className={styles.column}>
               <span className={styles.title}>HanoiGarden</span>
 
-              <embed src="http://www.hanoigarden.sk/images/Obedove%20menu%20pondelok_1.pdf" width="100%" height="600px">
+              <embed src={this.getMenuByDate(1)} width="100%" height="600px">
                 </embed>
-                <embed src="http://www.hanoigarden.sk/images/Obedove%20menu%20pondelok_2.pdf" width="100%" height="600px">
+                <embed src={this.getMenuByDate(2)} width="100%" height="600px">
                 </embed>
             </div>
           </div>
