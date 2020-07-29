a978e14a36a527c0ce6f41282dc8e8f886418cf1
Samuel <samuelorth62@gmail.com>
2020-07-29 15:27:13 +0200

    bs, nefunguje ten sprosty scroll

--- src/webparts/obedy/components/Restaurants/BeQuick.tsx
+++ src/webparts/obedy/components/Restaurants/BeQuick.tsx
@@ -4,6 +4,46 @@ import { IObedyProps } from '../IObedyProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
 export default class BeQuick extends React.Component {
+
+    handleClick () {
+
+      const dayNum = new Date(Date.now());
+
+      let day = dayNum.getDay();
+
+
+      var frame = document.getElementsByTagName("iframe")[0];
+
+
+      switch(day){
+        case 1:
+          frame.style.height="950px";
+          frame.style.marginTop="-550px";
+          break;
+        case 2:
+          document.getElementById("1").style.height="950px";
+          document.getElementById("1").style.marginTop="-550px";
+          break;
+        case 3:
+          frame.style.height="100px";
+          frame.scrollTo(0, 200);
+          break;
+        case 4:
+          document.getElementById("1").style.height="950px";
+          document.getElementById("1").style.marginTop="-150px";
+          break;
+        case 5:
+          document.getElementById("1").style.height="950px";
+          document.getElementById("1").style.marginTop="-550px";
+          break;
+        
+      }
+  }
+  componentDidMount(){
+    this.handleClick();
+  }
+  
+
   public render(): React.ReactElement {
     return (
       <div className={styles.obedy}>
@@ -11,7 +51,7 @@ export default class BeQuick extends React.Component {
           <div className={styles.row}>
             <div className={styles.column}>
               <span className={styles.title}>⏲️ BeQuick</span>
-              <iframe src="https://ranajky-obedy.sk/wp-content/uploads/Obedov%c3%a9-menu-27.7.-31.7.2020.pdf#toolbar=0&navpanes=0&scrollbar=0" width="100%" height="630px" scrolling="no">
+              <iframe id="1" src="https://ranajky-obedy.sk/wp-content/uploads/Obedov%c3%a9-menu-27.7.-31.7.2020.pdf#toolbar=0&navpanes=0&scrollbar=0" width="100%">
                 </iframe>
 
             </div>
