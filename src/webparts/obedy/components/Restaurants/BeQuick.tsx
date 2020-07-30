2f94eb23f49634334e5a30a254110c6f0663859a
William Brach <William.Brach@snt.sk>
2020-07-30 10:50:59 +0200

    adding dynamic url for bequick and yummy

--- src/webparts/obedy/components/Restaurants/BeQuick.tsx
+++ src/webparts/obedy/components/Restaurants/BeQuick.tsx
@@ -5,6 +5,20 @@ import { escape } from '@microsoft/sp-lodash-subset';
 import InfoBoard from '../InfoBoard';
 
 export default class BeQuick extends React.Component {
+
+  getUrlWithDates(){
+    const date = new Date(Date.now());
+    var day = date.getDate();
+    var weekIndex = date.getDay();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    var startWeekDay = day - weekIndex + 1;
+    var endWeekDay = startWeekDay + 4;
+    let url = "https://ranajky-obedy.sk/wp-content/uploads/Obedov%c3%a9-menu-"+startWeekDay+"."+month+".-"+endWeekDay+"."+month+"."+year+".pdf#toolbar=0&navpanes=0&scrollbar=0"
+    return url
+  }
+
+
   public render(): React.ReactElement {
     return (
       <div className={styles.obedy}>
@@ -18,7 +32,7 @@ export default class BeQuick extends React.Component {
             </div>
             <InfoBoard distance="0.6" link="https://goo.gl/maps/tXsTjYeBfm6vU9EX7" time="8" />
             <div className={styles.column}>
-              <iframe src="https://ranajky-obedy.sk/wp-content/uploads/Obedov%c3%a9-menu-27.7.-31.7.2020.pdf#toolbar=0&navpanes=0&scrollbar=0" width="100%" height="630px" scrolling="no">
+              <iframe src={this.getUrlWithDates()} width="100%" height="630px" scrolling="no">
               </iframe>
             </div>
           </div>
