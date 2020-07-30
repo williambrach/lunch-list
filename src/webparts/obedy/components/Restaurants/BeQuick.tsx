75ce5eb7fe0cd355c1a52c75b2560be3353c20a3
William Brach <William.Brach@snt.sk>
2020-07-30 10:12:49 +0200

    be-quick fix

--- src/webparts/obedy/components/Restaurants/BeQuick.tsx
+++ src/webparts/obedy/components/Restaurants/BeQuick.tsx
@@ -12,12 +12,12 @@ export default class BeQuick extends React.Component {
           <div className={styles.row}>
             <div className={styles.column}>
               <span className={styles.title}>⏲️ BeQuick</span>
+            </div>
               <InfoBoard distance="0.6" link="https://goo.gl/maps/tXsTjYeBfm6vU9EX7" time="8"/>
               <div className={styles.column}>
                 <iframe src="https://ranajky-obedy.sk/wp-content/uploads/Obedov%c3%a9-menu-27.7.-31.7.2020.pdf#toolbar=0&navpanes=0&scrollbar=0" width="100%" height="630px" scrolling="no">
                 </iframe>
               </div>
-            </div>
           </div>
         </div>
       </div>
