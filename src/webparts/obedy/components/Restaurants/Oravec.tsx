95673f8116ce273780915035bc9c6c95110d8edc
ramang22 <wibrach@gmail.com>
2020-09-03 13:28:07 +0200

    oravec working

--- src/webparts/obedy/components/Restaurants/Oravec.tsx
+++ src/webparts/obedy/components/Restaurants/Oravec.tsx
@@ -3,10 +3,49 @@ import styles from '../Obedy.module.scss';
 import { IObedyProps } from '../IObedyProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 import InfoBoard from '../InfoBoard';
+import LunchRow from '../LunchRow';
+import axios from 'axios';
+import { apiUrl } from '../ApiConstants'
+
+interface IOravec {
+
+}
+
+interface OravecState {
+  menu?: object;
+  loaded : boolean;
+}
 
 
-export default class Oravec extends React.Component {
+export default class Oravec extends React.Component<IOravec, OravecState> {
 
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: this.getMenuItems(),
+      loaded : false
+    }
+  }
+
+  async getApiCall(url: string): Promise<object> {
+    try {
+      const response = await axios.get(url);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getMenuItems() : Promise<object>{
+    let response = await this.getApiCall(apiUrl + "mlyn")
+    this.setState({
+      loaded : true,
+      menu: response['data']
+    })
+    
+    return response['data']
+  }
 
   public render(): React.ReactElement {
     return (
@@ -21,14 +60,20 @@ export default class Oravec extends React.Component {
             </div>
             <InfoBoard distance="1" link="https://goo.gl/maps/VEBvecVJHXQwCuKd8" time="12" />
 
-            <div className={styles.column}>
-              <div className={styles.iframeOravecDiv}>
-                <iframe scrolling="yes" src="https://menucka.sk/denne-menu/bratislava/gurmansky-dvor-u-oravca" className={styles.iframeOravec}>
-                </iframe>
-                <div className={styles.rectangle}></div>
-              </div>
-
-            </div>
+            {(this.state.loaded == true) ? (
+            <LunchRow menuNumber="Polievka" price={null} menu={this.state.menu['soup']} />
+            ) : (
+              <div></div>
+            )}
+            {(this.state.loaded == true) ? (
+               
+                this.state.menu['foods'].map((item, index) =>
+                <LunchRow menuNumber={"Menu " + (index + 1)} price={item['price']} menu={item['name']} />
+              )
+            ) : (
+                <div></div>
+              )
+            }
           </div>
         </div>
       </div>
