5a2040fa2a3247a9f1f0ef703791e1bc5eda4f0b
ramang22 <wibrach@gmail.com>
2020-09-03 10:29:25 +0200

    server serve bequick link

--- Obed_azure_app/Obed_azure_app/pdf/BeQuick.cs
+++ Obed_azure_app/Obed_azure_app/pdf/BeQuick.cs
@@ -1,5 +1,6 @@
 using HtmlAgilityPack;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,57 +15,13 @@ namespace Obed_azure_app.BeQuick
     public static async Task<string> startCrawlerasync()
     {
       var httpClient = new HttpClient();
-      var html = await httpClient.GetStringAsync();
+      var html = await httpClient.GetStringAsync("https://ranajky-obedy.sk/obedove-menu/");
       var htmlDocument = new HtmlDocument();
       htmlDocument.LoadHtml(html);
-      var divs = htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("daily-menu-row")).ToList();
-
-      int count = 0;
-      string soup = null;
-      string price = null;
-      var foods = new List<Food>();
-      var images = new List<string>();
-
-      foreach (var div in divs)
-      {
-        string rawText = div.InnerText.Trim();
-        string innerText = rawText.Replace("\t", " ");
-
-        if (count == 1)
-        {
-          soup = innerText;
-        }
-        else if (count == divs.Count)
-        {
-        }
-        else if (count != 0)
-        {
-
-
-          string[] words = innerText.Split(' ');
-          List<string> food = new List<string>();
-
-          int countArr = 0;
-          foreach (var word in words)
-          {
-            food.Add(word);
-            if (word.Contains("€"))
-            {
-              price = words[countArr - 1];
-              food.RemoveAt(countArr);
-              food.RemoveAt(countArr - 1);
-            }
-            countArr++;
-          }
-
-          foods.Add(new Food(String.Join(" ", food), price));
-        }
-        count++;
-      }
-
-      Menu menu = new Menu(soup, foods, null);
-      string json = JsonConvert.SerializeObject(menu);
-      return json;
+      var ar = htmlDocument.DocumentNode.SelectSingleNode("//*[@class='aio-tooltip 5f501e59ceabc']");
+      JObject o = new JObject();
+      o["link"] = ar.Attributes["href"].Value;
+      return o.ToString();
     }
   }
 }
